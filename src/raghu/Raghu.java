4b7daa107e612d8738ce3e71e8a3b964514eadfc
Raghu PC <Raghu PC@Raghu>
2020-09-23 13:15:58 +0530

    First Committ of Ragu package

--- src/raghu/Raghu.java
+++ src/raghu/Raghu.java
@@ -2,6 +2,7 @@ package raghu;
 
 public class Raghu {
 	public static void main(String[] args) {
-		System.out.println("Hye this is : Raghu package");
+		System.out.println("1.Hye this is : Raghu package");
+		System.out.println("2.Hello world of Raghu package");
 	}
-}//class Raghu
+}// class Raghu
